f6cb60c59ec50811f20e10ab9e9f2d1c189fe16c
glenchiridza <glenchiridz@gmail.com>
2024-01-15 23:08:01 +0200

    task api views

--- task/views.py
+++ task/views.py
@@ -1,9 +1,40 @@
-# from django.shortcuts import render
-#
-# from rest_framework import generics
-# from rest_framework import mixins
-#
-# class TaskListView(mixins.ListModelMixin,
-#                    mixins.CreateModelMixin,
-#                    generics.GenericAPIView):
-#     
+from django.shortcuts import render
+
+from rest_framework import generics
+from rest_framework import mixins
+
+from .models import Task
+from .serializers import TaskSerializer, TaskUpdateSerializer, TaskDeleteSerializer
+
+
+class TaskListView(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class TaskCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+
+class TaskUpdateView(generics.UpdateAPIView):
+    serializer_class = TaskUpdateSerializer
+    queryset = Task.objects.all()
+    lookup_field = 'id'
+
+
+class TaskDeleteView(generics.DestroyAPIView):
+    serializer_class = TaskDeleteSerializer
+    queryset = Task.objects.all()
+    lookup_field = 'id'
