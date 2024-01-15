eb190c4c0a307d531573f46fabee925244dc4373
glenchiridza <glenchiridz@gmail.com>
2024-01-15 22:41:47 +0200

    task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.TextField(unique=True)
+    is_reminder_on = models.BooleanField(default=False)
+    is_task_pending = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
